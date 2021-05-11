77ef038aa367e405e71c7d779c5391e2596813a2
Daniel Perrefort <djperrefort@pitt.edu>
2021-05-10 22:46:37 -0400

    Visualizer updates node colors in real time

--- egon/visualize.py
+++ egon/visualize.py
@@ -4,8 +4,10 @@ from pathlib import Path
 from typing import List
 
 import dash
+import dash.dependencies as ddep
+import dash_core_components as dcc
 import dash_cytoscape as cyto
-import dash_html_components as html
+import dash_html_components as dhtml
 import yaml
 
 from egon.nodes import AbstractNode, Source, Target
@@ -18,26 +20,49 @@ STYLE_PATH = Path(__file__).resolve().parent / 'default_style.yml'
 class PipelineCytoscape(cyto.Cytoscape):
     """A Dash compatible component for visualizing pipelines with cytoscape"""
 
-    def __init__(self, app: dash.Dash, pipeline: Pipeline, **kwargs) -> None:
+    def __init__(self, pipeline: Pipeline, **kwargs) -> None:
         """An interactive, cytoscape style plot of a constructed pipeline
 
         Args:
-            app: The main application for handling event callbacks
             pipeline: The pipeline that will be visualized
             kwargs: Any additional ``Cytoscape`` arguments except for ``elements``
         """
 
-        self._app = app
-        self._pipeline = pipeline
-
-        kwargs.setdefault('id', str(id(self)))
+        elements = []
+        self.pipeline_nodes = pipeline.get_nodes()
         stylesheet = kwargs.pop('stylesheet', self.default_stylesheet())
 
-        # Useful docs: https://dash.plotly.com/cytoscape
-        super().__init__(elements=self._get_pipeline_elements(), **kwargs)
-        self.stylesheet = stylesheet
+        # Iterate over pipeline nodes in an arbitrary order O(n)
+        for node in pipeline.get_nodes():
+            # Identify and label the node on the plot
+            node_id = str(id(node))
+            elements.append({
+                'data': {'id': node_id, 'label': node.name},
+                'classes': self.node_class(node)
+            })
 
-    def default_stylesheet(self) -> List[dict]:
+            # Draw an arrow from the node to any downstream nodes
+            for downstream in node.downstream_nodes():
+                downstream_id = str(id(downstream))
+                elements.append(
+                    {'data': {'source': node_id, 'target': downstream_id}}
+                )
+
+            # Add individual CSS styling the current node
+            # We guarantee that these values are at the end of the style sheet
+            stylesheet.append(
+                {
+                    'selector': f'[id == {node_id}]',
+                    'style': {
+                        'background-color': 'grey'
+                    }
+                }
+            )
+
+        super().__init__(elements=elements, stylesheet=stylesheet, **kwargs)
+
+    @staticmethod
+    def default_stylesheet() -> List[dict]:
         """Return a copy of the default style sheet
 
         Return:
@@ -68,27 +93,6 @@ class PipelineCytoscape(cyto.Cytoscape):
 
         return 'default_node'
 
-    def _get_pipeline_elements(self) -> List[dict]:
-        """Return the pipeline elements to monitor"""
-
-        # Iterate over pipeline nodes in an arbitrary order O(n)
-        elements = []
-        for node in self._pipeline.get_nodes():
-            node_id = str(id(node))
-            elements.append({
-                'data': {'id': node_id, 'label': node.name},
-                'classes': self.node_class(node)
-            })
-
-            # Draw an arrow from the node to any downstream nodes
-            for downstream in node.downstream_nodes():
-                downstream_id = str(id(downstream))
-                elements.append(
-                    {'data': {'source': node_id, 'target': downstream_id}}
-                )
-
-        return elements
-
 
 class Visualizer(dash.Dash):
     """Application for visualizing an analysis pipeline"""
@@ -104,7 +108,7 @@ class Visualizer(dash.Dash):
         pipeline.validate()
         self.layout = self._build_html(pipeline)
 
-    def _build_html(self, pipeline: Pipeline) -> dash.development.base_component.Component:
+    def _build_html(self, pipeline: Pipeline, update_interval: int = 2) -> dhtml.Div:
         """Create the HTML content to be displayed by the app
 
         Args:
@@ -114,7 +118,21 @@ class Visualizer(dash.Dash):
             An HTML component
         """
 
-        return html.Div([
-            html.H1('Pipeline Overview', id='overview-header'),
-            PipelineCytoscape(self, pipeline)
+        cytoscape = PipelineCytoscape(pipeline, id='pipeline-cyto')
+        update_interval_ms = update_interval * 1000  # The interval in milliseconds
+        interval = dcc.Interval(id="interval", interval=update_interval_ms)
+
+        @self.callback(ddep.Output('pipeline-cyto', 'stylesheet'), ddep.Input('interval', 'n_intervals'))
+        def update_cytoscape_node_colors(*args) -> List[dict]:
+            style = cytoscape.stylesheet
+            for i, node in enumerate(cytoscape.pipeline_nodes):
+                color = 'black' if node.node_finished else 'green'
+                style[-i]['style']['background-color'] = color
+
+            return style
+
+        return dhtml.Div([
+            dhtml.H1('Pipeline Overview', id='overview-header'),
+            cytoscape,
+            interval,
         ], id='overview-section')
