9da855e466345a4917ee7a1efae4e2256bc15e99
Daniel <djperrefort@gmail.com>
2021-07-08 07:06:11 -0400

    Block resource warnings from ray in test suite init

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,8 @@
 """Test suite for the Egon package."""
 
+import logging
 import unittest
+import warnings
 
 import ray
 
@@ -10,7 +12,11 @@ OLD_TEST_RUN = unittest.result.TestResult.startTestRun
 def startTestRun(self) -> None:
     """Instantiate ray so test can be run against a mock ray cluster"""
 
-    ray.init(num_cpus=4)
+    # Block resource warnings raised by a bug in some ray versions
+    # See https://github.com/ray-project/ray/issues/9546
+    warnings.filterwarnings('ignore', category=ResourceWarning, module='ray')
+
+    ray.init(num_cpus=4, logging_level=logging.ERROR)
 
 
 unittest.result.TestResult.startTestRun = startTestRun
