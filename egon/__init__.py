204a9de922fba8a24bf2bf5274748c6365fbe1fa
Daniel Perrefort <djperrefort@pitt.edu>
2021-07-29 10:46:19 -0400

    Minor version bump

--- egon/__init__.py
+++ egon/__init__.py
@@ -3,7 +3,7 @@ analysis pipelines. See the online documentation at
 https://mwvgroup.github.io/Egon/
 """
 
-__version__ = '0.5.0'
+__version__ = '0.6.0'
 __author__ = 'MWV Research Group'
 __license__ = 'GPL 3.0'
 
